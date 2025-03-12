27f6537b0a1f5852ee1c91abba9ea99f892b2038
Kaydev-cmd <keamogetswingwako2001@gmail.com>
2025-03-12 16:07:14 -0700

    Refactored KeyFeaturesCards components, improved reusability, dynamic rendering, and interactivity

--- frontend/src/components/KeyFeaturesCards.jsx
+++ frontend/src/components/KeyFeaturesCards.jsx
@@ -1,60 +1,49 @@
 import React from "react";
 
-export const KeyFeaturesCards = () => {
+const keyFeatures = [
+  {
+    id: 1,
+    imgSrc: "/images/home/Group 9239.png",
+    altText: "Location",
+    title: "We offer best services",
+    description: "Lorem Ipsum is not simply random text",
+  },
+  {
+    id: 2,
+    imgSrc: "/images/home/Group 9240.png",
+    altText: "Calender",
+    title: "Schedule your trip",
+    description: "It has roots in a piece of classical",
+  },
+  {
+    id: 3,
+    imgSrc: "/images/home/Group 9241.png",
+    altText: "Coupon",
+    title: "Get discounted coupons",
+    description: "Lorem Ipsum is not simply random text",
+  },
+];
+
+const FeatureCard = ({ imgSrc, altText, title, description }) => {
   return (
-    <>
-      <div className="flex flex-col gap-8 xl:gap-4">
-        {/* Card */}
-        <div className="flex items-center justify-around gap-8 p-4 border rounded-3xl">
-          <img
-            src="/images/home/Group 9239.png"
-            alt="Location"
-            className="lg:w-16"
-          />
-          <div className="right-wrapper">
-            <h5 className="font-bold text-[18px] xs:text-[14px]">
-              We offer best services
-            </h5>
-            <p className="text-[#191825] xs:text-[12px]">
-              Lorem Ipsum is not simply random text
-            </p>
-          </div>
-        </div>
-
-        {/* Card */}
-        <div className="flex items-center justify-around gap-8 p-4 border rounded-3xl">
-          <img
-            src="/images/home/Group 9240.png"
-            alt="Calender"
-            className="lg:w-16"
-          />
-          <div className="right-wrapper">
-            <h5 className="font-bold text-[18px] xs:text-[14px]">
-              Schedule your trip
-            </h5>
-            <p className="text-[#191825] xs:text-[12px]">
-              It has roots in a piece of classical
-            </p>
-          </div>
-        </div>
-
-        {/* Card */}
-        <div className="flex items-center justify-around gap-8 p-4 border rounded-3xl">
-          <img
-            src="/images/home/Group 9241.png"
-            alt="Coupon"
-            className="lg:w-16"
-          />
-          <div className="right-wrapper">
-            <h5 className="font-bold text-[18px] xs:text-[14px]">
-              Get discounted coupons
-            </h5>
-            <p className="text-[#191825] xs:text-[12px]">
-              Lorem Ipsum is not simply random text
-            </p>
-          </div>
-        </div>
+    <div className="flex items-center justify-around gap-8 p-4 transition-transform duration-300 border rounded-3xl hover:scale-105">
+      <img src={imgSrc} alt={altText} className="lg:w-16" />
+      <div className="right-wrapper">
+        <h5 className="font-bold text-[18px] xs:text-[14px]">{title}</h5>
+        <p className="text-[#191825] xs:text-[12px]">{description}</p>
       </div>
-    </>
+    </div>
   );
 };
+
+export const KeyFeaturesCards = () => {
+  return (
+    <div className="flex flex-col gap-8 xl:gap-4">
+      {keyFeatures.map((feature) => (
+        <FeatureCard key={feature.id} {...feature} />
+      ))}
+    </div>
+  );
+};
+
+export default KeyFeaturesCards;
